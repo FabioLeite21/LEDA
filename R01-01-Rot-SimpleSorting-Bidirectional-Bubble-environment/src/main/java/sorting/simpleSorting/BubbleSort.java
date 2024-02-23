3090f6b63097c0bb4b8b6837819aaa09c44c5486
seu FabioLeite21 <fabio.araujo.leite.medeiros@ccc.ufcg.edu.br>
2024-02-23 11:28:02 -0300

    Selection Sort e Selection Sort Recursivo

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/BubbleSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -1,6 +1,7 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The bubble sort algorithm iterates over the array multiple times, pushing big
@@ -11,7 +12,21 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if(array.length != 0 && leftIndex != rightIndex && leftIndex > 0){
+			boolean swapped = true;
+			int n = rightIndex;
+			while(swapped){
+				swapped = false;
+				for(int i = leftIndex; i < n; i++){
+					if(array[i].compareTo(array[i+1]) > 0){
+						Util.swap(array, i, i+1);
+						swapped = true;
+					}
+				}
+				if(swapped)n--;
+
+			}
+			
+		}
 	}
 }
