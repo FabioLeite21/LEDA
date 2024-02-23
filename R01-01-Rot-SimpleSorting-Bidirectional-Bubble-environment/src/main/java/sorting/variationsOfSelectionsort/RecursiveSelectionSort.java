65a23c777d12566f62ec663f16835303845c7748
seu FabioLeite21 <fabio.araujo.leite.medeiros@ccc.ufcg.edu.br>
2024-02-23 10:59:07 -0300

    Selection Sort e Selection Sort Recursivo

---
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfSelectionsort/RecursiveSelectionSort.java
@@ -0,0 +1,36 @@
+package sorting.variationsOfSelectionsort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+public class RecursiveSelectionSort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+
+	/**
+	 * Implementação recursiva do selection sort. Você deve implementar apenas
+	 * esse método sem usar nenhum outro método auxiliar (exceto
+	 * Util.swap(array,int,int)). Para isso, tente definir o caso base do
+	 * algoritmo e depois o caso indutivo, que reduz o problema para uma entrada
+	 * menor em uma chamada recursiva. Seu algoritmo deve ter complexidade
+	 * quadrática O(n^2).
+	 */
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if(array.length != 0 && leftIndex != rightIndex && leftIndex > 0){
+			if(leftIndex < rightIndex){
+				int index_menor = leftIndex;
+
+				for(int j = leftIndex + 1; j < rightIndex + 1; j++){
+					if(array[index_menor].compareTo(array[j]) > 0){
+						index_menor = j;
+					}
+				}
+				Util.swap(array, leftIndex, index_menor);
+				sort(array, leftIndex + 1, rightIndex);
+
+			}
+		}
+
+	}
+
+}
